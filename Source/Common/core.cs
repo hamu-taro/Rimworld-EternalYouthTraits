5d4350e6d872f2cbbed48cf3df79cf789c8d4b4a
hamu-taro <hamutaro@etheldreda.com>
2019-03-07 22:11:50 +0900

    [add] 不老特性を追加しました。

---
+++ Source/Common/core.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Harmony;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace EternalYouthTraits
+{
+
+	public static partial class core
+	{
+		public static readonly TraitDef EternalYouth = TraitDef.Named("EternalYouth");
+
+		public static bool has_traits(Pawn pawn)
+		{
+			return pawn?.story?.traits != null;
+		}
+
+		public static bool is_human(Pawn pawn)
+		{
+			if (pawn == null) return false;
+
+			return pawn.RaceProps.Humanlike;
+		}
+
+		public static bool has_eternalYouth(Pawn pawn)
+		{
+			if (!has_traits(pawn)) { return false; }
+			return is_human(pawn) && pawn.story.traits.HasTrait(TraitDef.Named("EternalYouth"));
+		}
+	}
+}
---
+++ Source/Harmony/patches.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using RimWorld;
+using Verse;
+using Verse.AI.Group;
+using System.Reflection;
+
+namespace EternalYouthTraits
+{
+	[HarmonyPatch(typeof(Pawn_AgeTracker))]
+	[HarmonyPatch(nameof(Pawn_AgeTracker.AgeTick))]
+	static class Patches_AgeTracker_AgeTick_aliceDoll
+	{
+		[HarmonyPrefix]
+		static bool prefix_AgeTick(Pawn_AgeTracker __instance)
+		{
+			Pawn p = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
+			if(core.has_eternalYouth(p)) {
+				return false; //return false to skip execution of the original.
+			}
+			return true;
+		}
+	}
+
+	[StaticConstructorOnStartup]
+	internal static class First
+	{
+		static First()
+		{
+			var har = HarmonyInstance.Create("EternalYouthTraits");
+			har.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
