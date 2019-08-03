7ddfbbdc3a99766e932da9d09aee1ebc70848468
hamu-taro <hamutaro@etheldreda.com>
2019-08-03 13:04:20 +0900

    1.0.2 - 不死特性と不老不死特性を追加しました

--- Source/Common/core.cs
+++ Source/Common/core.cs
@@ -11,10 +11,11 @@ namespace EternalYouthTraits
 	public static partial class core
 	{
 		public static readonly TraitDef TraifDefEternalYouth = TraitDef.Named("EternalYouth");
-		public static readonly TraitDef TraifDefEternalImmortal = TraitDef.Named("EternalImortary");
+		public static readonly TraitDef TraifDefEternalImmortal = TraitDef.Named("EternalImmortal");
+		public static readonly TraitDef TraifDefEternalImmortary = TraitDef.Named("EternalImmortary");
 
-//		public static HediffDef hediffDefEternalYouth = HediffDef.Named("hediffOfEternalYouth");
-//		public static HediffDef hediffDefEternalImortary = HediffDef.Named("hediffOfEternalImortary");
+		//		public static HediffDef hediffDefEternalYouth = HediffDef.Named("hediffOfEternalYouth");
+		//		public static HediffDef hediffDefEternalImortary = HediffDef.Named("hediffOfEternalImortary");
 
 		public static bool has_traits(Pawn pawn)
 		{
@@ -39,5 +40,13 @@ namespace EternalYouthTraits
 			if (!has_traits(pawn)) { return false; }
 			return is_human(pawn) && pawn.story.traits.HasTrait(TraifDefEternalImmortal);
 		}
+
+		public static bool has_eternalImmortary(Pawn pawn)
+		{
+			if (!has_traits(pawn)) { return false; }
+			return is_human(pawn) && pawn.story.traits.HasTrait(TraifDefEternalImmortary);
+		}
+		
+
 	}
 }
--- Source/Harmony/patches.cs
+++ Source/Harmony/patches.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 namespace EternalYouthTraits
 {
 	[StaticConstructorOnStartup]
-	internal class HarmonyPatches
+	static partial class HarmonyPatches
 	{
 		private static readonly Type patchType = typeof(HarmonyPatches);
 
@@ -17,32 +17,40 @@ namespace EternalYouthTraits
 		{
 			HarmonyInstance harmonyInstance = HarmonyInstance.Create(id: "EternalYouthTraits");
 
+			#region EternalYouthTraits            
+//			Log.Message("EternalYouthTraits");
+
 			harmonyInstance.Patch(original: AccessTools.Method(type: typeof(Pawn_AgeTracker), name: "AgeTick"),
 					prefix: new HarmonyMethod(type: patchType, name: nameof(prefix_AgeTick_EternalYouthTraits)));
 
-#if __EY_UNREF__
+			#endregion
+
+			#region EternalImortaly
+//			Log.Message("EternalImortaly");
+
+#if __EY_UNUSE__
 			harmonyInstance.Patch(original: AccessTools.Method(type: typeof(Pawn), name: "Kill"),
-					prefix: new HarmonyMethod(type: patchType, name: nameof(prefix_Kill_EternalImmortalityTraits)));
+					prefix: new HarmonyMethod(typeof(HarmonyPatches), name: nameof(prefix_Kill_EternalImmortalityTraits)));
 #endif
 
 			harmonyInstance.Patch(original: AccessTools.Method(type: typeof(Pawn_HealthTracker), name: "ShouldBeDead"),
 					prefix: new HarmonyMethod(type: patchType, name: nameof(prefix_ShouldBeDead_EternalImmortalityTraits)));
-
+			#endregion
 		}
 
 		// -------------------------------------------------------------------------
 		// 不老制御
-		static bool prefix_AgeTick_EternalYouthTraits(Pawn_AgeTracker __instance, ref bool __result)
+		public static bool prefix_AgeTick_EternalYouthTraits(Pawn_AgeTracker __instance)
 		{
-			Pawn p = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
+			Pawn pawn = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
 
-			if (core.has_eternalYouth(p)) {
+			if (core.has_eternalYouth(pawn) || core.has_eternalImmortary(pawn)) {
 				return false; //return false to skip execution of the original.
 			}
 			return true;
 		}
 
-#if __EY_UNREF__
+#if __EY_UNUSE__
 		// Kill 関数ではなく、ShouldBeDead()でやるほうが合理的か？
 		// -------------------------------------------------------------------------
 		// 不死制御
@@ -75,19 +83,22 @@ namespace EternalYouthTraits
 		// -------------------------------------------------------------------------
 		// 不死制御
 		// 死ぬ判定のHOOK
-		static bool prefix_ShouldBeDead_EternalImmortalityTraits(Pawn_HealthTracker __instance, ref bool __result)
+		public static bool prefix_ShouldBeDead_EternalImmortalityTraits(Pawn_HealthTracker __instance, ref bool __result)
 		{
 			Pawn pawn = Traverse.Create(root: __instance).Field(name: "pawn").GetValue<Pawn>();
 
 			// -------------------------------------------------------------------------
 			// 処理除外判定群
 			// プレイヤーファクション以外は対象外
-			if (pawn == null) return true;
-			if (!pawn.Faction.IsPlayer) return true;
-			if (!core.has_eternalImmortal(pawn)) return true;
+			if (!pawn?.Faction?.IsPlayer ?? true) return true;
+			if (!core.has_eternalImmortal(pawn) && !core.has_eternalImmortary(pawn)) return true;
 
 			__result = false;
 			return false; //return false to skip execution of the original.
 		}
 	}
+
+
+
+
 }
